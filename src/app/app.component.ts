c2bf9ee51cc4ca352a642c2a9c9752c60951b430
imandzankovic <fruit.10@hotmail.com>
2020-02-01 16:23:45 +0100

    [addMarker] From for adding new Marker done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { MarkerManager } from "@agm/core";
+import { MouseEvent } from "@agm/core";
 
 @Component({
   selector: "app-root",
@@ -12,34 +14,86 @@ export class AppComponent {
   // Start position
   lat: number = 51.673858;
   lng: number = 7.815982;
+  
+  // Values
+  markerName:string;
+  markerLat:string;
+  markerLng:string;
+  markerDraggable:string;
+  
   // Markers
-   
   markers: marker[] = [
-	  {
-		  lat: 51.673858,
-		  lng: 7.815982,
-		  label: 'A',
-		  draggable: true
-	  },
-	  {
-		  lat: 51.373858,
-		  lng: 7.215982,
-		  label: 'B',
-		  draggable: false
-	  },
-	  {
-		  lat: 51.723858,
-		  lng: 7.895982,
-		  label: 'C',
-		  draggable: true
-	  }
-  ]
+    {
+      lat: 51.673858,
+      lng: 7.815982,
+      label: "A",
+      draggable: true
+    },
+    {
+      lat: 51.373858,
+      lng: 7.215982,
+      label: "B",
+      draggable: false
+    },
+    {
+      lat: 51.723858,
+      lng: 7.895982,
+      label: "C",
+      draggable: true
+    }
+  ];
+
+  mapClicked($event: MouseEvent) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: false
+    });
+  }
+
+  markerDragEnd(marker: any, $event: any) {
+    console.log("drag end", marker.label, $event);
+
+    var updMarker = {
+      name: marker.name,
+      lat: parseFloat(marker.lat),
+      lng: parseFloat(marker.lng),
+      draggable: false
+    };
+
+    var newLat = $event.coords.lat;
+    var newLng = $event.coords.lng;
+  }
+
+  addMarker(){
+    console.log('adding marker');
+    if(this.markerDraggable == 'yes'){
+      var isDraggable = true;
+
+    }else{
+      var isDraggable = false;
+    }
+
+    var newMarker={
+      name:this.markerName,
+      lat: parseFloat(this.markerLat),
+      lng:parseFloat(this.markerLng),
+      draggable:isDraggable
+    }
+
+
+    this.markers.push(newMarker);
+  }
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
+  }
 }
 
 // just an interface for type safety.
 interface marker {
-	lat: number;
-	lng: number;
-	label?: string;
-	draggable: boolean;
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AgmCoreModule } from '@agm/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDSLDVsHbuo6EfXHHDILYs9OA_d8Q046vE'
     }),
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
